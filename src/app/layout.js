e698614dbc7a194b315ab261595dd7aa019213a6
Eduard Povierin <edpovieri@gmail.com>
2024-10-05 08:38:33 +0200

    Fix pwa

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,10 +16,17 @@ export const metadata = {
    manifest: '/manifest.json',
    appleWebApp: {
       capable: true,
-      display: 'standalone',
+      display: 'fullscreen',
       statusBarStyle: 'black-translucent',
       title: APP_DEFAULT_TITLE,
    },
+}
+export const viewport = {
+   width: 'device-width',
+   initialScale: 1,
+   maximumScale: 1,
+   viewportFit: 'cover',
+   userScalable: false,
 }
 
 export default function RootLayout({ children }) {
@@ -27,12 +34,6 @@ export default function RootLayout({ children }) {
       <html lang='en'>
          <head>
             <meta charSet='UTF-8' />
-            <meta name='viewport' content='width=device-width, initial-scale=1.0, viewport-fit=cover' />
-            <meta name='mobile-web-app-capable' content='yes' />
-            <meta name='apple-mobile-web-app-capable' content='yes' />
-            <meta name='apple-mobile-web-app-status-bar-style' content='black-translucent' />
-            <meta name='description' content={metadata.description} />
-            <title>{metadata.title.default}</title>
             <link rel='icon' href='/favicon.ico' />
             <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
             <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
